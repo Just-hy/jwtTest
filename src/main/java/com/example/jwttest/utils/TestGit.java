1ee3e37704a3e590556966da1e21968b49d5c65a
huangyi <525427312@qq.com>
2022-10-21 13:24:38 +0800

    add test4

--- src/main/java/com/example/jwttest/utils/TestGit.java
+++ src/main/java/com/example/jwttest/utils/TestGit.java
@@ -7,4 +7,10 @@ package com.example.jwttest.utils;
  * @Version:  1.0
  */
 public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+    }
 }
