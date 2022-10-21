74f7703802224c737299d14fa2222ecc03a6a09f
huangyi <525427312@qq.com>
2022-10-21 13:31:02 +0800

    add hot-fix-test

--- src/main/java/com/example/jwttest/utils/TestGit.java
+++ src/main/java/com/example/jwttest/utils/TestGit.java
@@ -9,6 +9,6 @@ package com.example.jwttest.utils;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("test");
-        System.out.println("hot-fix");
+        System.out.println("hot-fix-test");
     }
 }
