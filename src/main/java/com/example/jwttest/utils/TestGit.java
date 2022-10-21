45b08bef623f24184cabd467fc4068ffd78db845
hy <525427312@qq.com>
2022-10-21 07:41:17 +0000

    update src/main/java/com/example/jwttest/utils/TestGit.java.
    Signed-off-by: hy <525427312@qq.com>

--- src/main/java/com/example/jwttest/utils/TestGit.java
+++ src/main/java/com/example/jwttest/utils/TestGit.java
@@ -14,5 +14,6 @@ public class TestGit {
         System.out.println("hot-fix-test");
         System.out.println("pull test");
         System.out.println("gitee test");
+        System.out.println("gitee test pull");
     }
 }
