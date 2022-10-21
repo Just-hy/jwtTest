32d0c327fda6280c22a0d75aac52490f8fc5883c
huangyi <525427312@qq.com>
2022-10-21 15:30:48 +0800

    添加swagger-ui的跨域问题解决

--- src/main/java/com/example/jwttest/config/GlobalCorsConfig.java
+++ src/main/java/com/example/jwttest/config/GlobalCorsConfig.java
@@ -1,32 +1,39 @@
 package com.example.jwttest.config;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 //跨域问题解决
 @Configuration
-
 public class GlobalCorsConfig extends WebMvcConfigurationSupport {
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/resources/")
                 .addResourceLocations("classpath:/static/")
                 .addResourceLocations("classpath:/public/");
+
+        // 设置swagger静态资源访问（跨域问题导致swagger不能正常访问）
+        // 解决swaggerui不显示问题
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+
         super.addResourceHandlers(registry);
     }
 
 
     @Bean
-    public FilterRegistrationBean   corsFilter() {
+    public FilterRegistrationBean corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         // 设置允许跨域请求的域名
         config.addAllowedOrigin("*");
@@ -37,10 +44,10 @@ public class GlobalCorsConfig extends WebMvcConfigurationSupport {
         // 允许任何头
         config.addAllowedHeader("*");
         //预检请求头信息描述
-       // config.addExposedHeader("token");
+        // config.addExposedHeader("token");
         UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
         configSource.registerCorsConfiguration("/**", config);
-        CorsFilter corsFilter= new CorsFilter(configSource);
+        CorsFilter corsFilter = new CorsFilter(configSource);
         FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(configSource));
         bean.setOrder(Ordered.HIGHEST_PRECEDENCE); // 优先级最高
         return bean;
